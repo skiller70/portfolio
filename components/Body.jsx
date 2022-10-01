ec3692d0aed9855ef29695aab7675db572cce47a
skiller70 <singhlucky007007@gmail.com>
2022-10-01 09:48:06 +0530

    deploy

--- components/Body.jsx
+++ components/Body.jsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const Body = ({ data }) => {
   return (
-    <div>
+    <>
       {/* LANDING PAGE */}
-      <div className="flex-col md:flex-row flex w-full h-[100vh] ">
+      <div className="flex-col border-red-600 border-2 md:flex-row flex w-full h-[100vh] md:h-[100vh] ">
         <div className="flex  justify-center  items-center md:flex-grow">
           <div className=" p-2 md:p-0 mb-2 flex flex-col">
             <motion.h1
@@ -56,7 +56,7 @@ const Body = ({ data }) => {
 
       {/* ABOUT ME */}
 
-      <div className=" mt-[35%] md:mt-[-8%] flex flex-col  md:flex-row     h-[100vh] w-screen">
+      <div className="border-green-600 border-2   flex flex-col  md:flex-row    h-[70vh] md:h-[100vh]  w-screen">
         <div className="order-last md:order-first   justify-center flex items-center  md:pl-12 pl-0 ">
           <Image src="/aboutMe.png" width="500" height="450"></Image>
         </div>
@@ -82,83 +82,112 @@ const Body = ({ data }) => {
           width="1500"
           height="800"
         ></Image> */}
+      <div className=" mt-[10%]   md:mt-[0%]        flex flex-grow flex-row justify-center  text-[2rem] ">
+        My Skills
+      </div>
 
-      <div className="w-full border-red-500 border-2 h-[50vh]">
-        <div className="border-2">
-          <div className="flex">
-            <svg
-              className=" w-[3rem] h-[3rem]"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"
-              />
-            </svg>
-            <h1>asdasd</h1>
+      <div className="w-full flex  flex-col md:flex-row border-red-500 border-2 h-[100vh]">
+        <div className="border-2 flex  flex-col  flex-grow">
+          {/* 2BOX */}
+          <div className="flex-col   items-center flex-grow flex md:flex-row">
+            <div className="w-[30%] flex justify-center ">
+             
+                <svg
+                  className=" w-[3rem] h-[3rem]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"
+                  />
+                </svg>
+              </div>
+              <div className=" flex justify-center md:justify-start w-[100%] md:w-[50%]">
+                <h1>aaasadasasdaasdasdaaaa <br></br>aaaasaasdasdasaaaaaasdasdasdasasd</h1>
+              </div>
+           
           </div>
-          <div className="flex">
-            <svg
-              className=" w-[3rem] h-[3rem]"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
-              />
-            </svg>
 
-            <h1>asdasdasd</h1>
-          </div>
-          <div className="flex">
-            <svg
+          <div className="flex-col items-center flex-grow flex md:flex-row">
+            <div className="w-[30%] flex justify-center ">
+              <svg
                 className=" w-[3rem] h-[3rem]"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
-              />
-            </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
+                />
+              </svg>
+            </div>
+
+            <div className="flex justify-center md:justify-start w-[100%] md:w-[50%]">
+              {" "}
+              <h1>asdasaaaaaaaaaaaaaadasd</h1>
+            </div>
+          </div>
+          <div className="flex-col items-center flex-grow flex md:flex-row">
+            <div className="md:w-[30%] flex justify-center ">
+              <svg
+                className=" w-[3rem] h-[3rem] "
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                />
+              </svg>
+            </div>
+            <div className=" flex justify-center md:justify-start w-[100%] md:w-[50%] "  >
+              <h1>asdaaaaaaaaaaaaaaaaaaaaaaaaaasd</h1>
+            </div>
           </div>
 
-      
         </div>
 
 
-        <div>
-          <div>
-          <Image></Image>
+        {/*  image 2BOX */}
+
+        <div className="flex-grow flex  gap-y-3 md:gap-y-16  items-center flex-col  justify-center ">
+          <div className="flex gap-x-2 md:gap-x-16    ">
+            <Image src="/html.png" height="110" width="110"></Image>
+
+            <Image src="/css.png" height="110" width="110"></Image>
+
+            <Image src="/javascript.png" height="110" width="110"></Image>
           </div>
-          <div>
-            
-            </div>
-            <div>
-            
-            </div>
+          <div className="flex gap-x-2 md:gap-x-16  ">
+            <Image src="/bootstrap.png" height="110" width="110"></Image>
 
+            <Image src="/tailwind.png" height="110" width="110"></Image>
 
+            <Image src="/reactjs.png" height="110" width="110"></Image>
+          </div>
+          <div className="flex gap-x-2 md:gap-x-16  ">
+            <Image src="/nodejs.png" height="110" width="110"></Image>
 
+            <Image src="/expressjs.png" height="80" width="80"></Image>
+
+            <Image src="/mongodb.png" height="110" width="110"></Image>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Body from "../components/Body";
 import Header from "../components/Header";
-import axios from "axios"
-import Image from "next/image";
+
 
 export default function Home() {
 
