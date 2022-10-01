a017d08f7eec4be3ffdc73f6b21f00ba4b34e02e
skiller70 <singhlucky007007@gmail.com>
2022-10-01 10:01:14 +0530

    fix dynamic

--- pages/test/[test].jsx
+++ pages/test/[test].jsx
@@ -1,25 +1,25 @@
-import {useRouter} from "next/router"
-import Head from "next/head";
+// import {useRouter} from "next/router"
+// import Head from "next/head";
 
 
 
 
 
-const index = (props) => {
+// const index = (props) => {
     
-    const router = useRouter();
-    const params = router.query.test;
+//     const router = useRouter();
+//     const params = router.query.test;
   
-    return (
-        <>
-     <Head> 
-        <title>{params}</title>
-     </Head>
-        <div>
-            hello this is test route {params}
-        </div>
-        </>
-    );
-};
+//     return (
+//         <>
+//      <Head> 
+//         <title>{params}</title>
+//      </Head>
+//         <div>
+//             hello this is test route {params}
+//         </div>
+//         </>
+//     );
+// };
 
-export default index;
+// export default index;
